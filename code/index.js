cbe3e24037476be222ec25ca26fb54d7aa573942
Michael <mdufek.660@gmail.com>
2021-11-24 16:49:42 -0500

    Update to functionality

--- code/index.js
+++ code/index.js
@@ -4,6 +4,7 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'process';
 
 
+
 const dictionary = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const rl=readline.createInterface({input,output})
 let continueGame=true
@@ -19,21 +20,22 @@ let getWord = () => {
 //This function takes in an int from 0-6, which represents how many wrong guesses the user has submitted
 //It returns a string that corresponds to what state the hanged man should be in according to mistakes
 let getDrawing = (wg) =>{
+	let returnString=""
 	switch(wg){
 		case 0:
-			return "\n"
+			return "_________\n\|\| \/\/\n\|\|\/\/\ \n\| \/\ \n\|\|\ \n\|\|\ \n\|\|_______"
 		case 1:
-			return " 0 \n"
+			return "_________\n\|\| \/\/  |\n\|\|\/\/\   0\n\| \/\ \n\|\|\ \n\|\|\ \n\|\|_______"
 		case 2:
-			return " 0 \n | \n | \n"
+			return "_________\n\|\| \/\/  |\n\|\|\/\/\   0\n\| \/\    |\n\|\|     |\n\|\| \n\|\|_______"
 		case 3:
-			return " 0 \n\\| \n | \n"
+			return "_________\n\|\| \/\/  |\n\|\|\/\/\   0\n\| \/\   \\|\n\|\|     |\n\|\| \n\|\|_______"
 		case 4:
-			return " 0 \n\\|/ \n | \n"
+			return "_________\n\|\| \/\/  |\n\|\|\/\/\   0\n\| \/\   \\|/\n\|\|     |\n\|\| \n\|\|_______"
 		case 5: 
-			return " 0 \n\\|/ \n | \n/ \n"
+			return "_________\n\|\| \/\/  |\n\|\|\/\/\   0\n\| \/\   \\|/\n\|\|     |\n\|\|    /\n\|\|_______"
 		case 6:
-			return " 0 \n\\|/ \n | \n/ \\\n"
+			return "_________\n\|\| \/\/  |\n\|\|\/\/\   0\n\| \/\   \\|/\n\|\|     |\n\|\|    / \\\n\|\|_______"
 		default:
 			return "\n"
 	}
@@ -42,6 +44,7 @@ let getDrawing = (wg) =>{
 //This functions prompts the user for an input with a variable display
 let getUserInput = async (textToDisplay) => {
 	const answer = await rl.question(textToDisplay)
+	await new Promise(r => setTimeout(r, 20));
 	return answer
 }
 
@@ -63,9 +66,17 @@ let replaceChar = (phrase, index, character) =>{
 	return phrase.substring(0,index)+character+phrase.substring(index+1)
 }
 
-while(continueGame){
-	console.log("Welcome to Hangman!\nPress Ctrl+C to exit at any time\n")
 
+
+
+let wonGames=0;
+let lostGames=0;
+
+while(continueGame){
+	console.log("About to clear")
+	console.clear();
+	console.log("Welcome to Hangman!\nPress Ctrl+C to exit at any time\nGames won: "+wonGames+ " Games lost: "+lostGames)
+	console.log(getDrawing(0))
 	let solved=false
 	let guessedLetters=[]
 	let currentWord=getWord()
@@ -77,22 +88,32 @@ while(continueGame){
 	for(let i=0; i<currentWord.length; i++){
 		obscuredCW+="_ "
 	}
+	
+	let cheat=false;
 	console.log(obscuredCW)
 	while(!solved && wrongGuesses<6){
-
 		//the following for loop is a method of entry validation to ensure only correct inputs get through
 		let validEntry=false;
+
 		while(!validEntry){
 			guess=await getUserInput("Please enter a new guess: ")
-			guess=guess[0].toLowerCase()
-			if(dictionary.includes(guess) && !guessedLetters.includes(guess)){
-				validEntry=true
+			await new Promise(r => setTimeout(r, 2));
+			if(guess=="Cheat code"){
+				console.log(currentWord)
+				cheat = true;
 			}
 			else{
-				console.log("Your entry of \""+guess+"\" is not valid. Try again")
+
+				guess=guess[0].toLowerCase()
+				if(dictionary.includes(guess) && !guessedLetters.includes(guess)){
+					validEntry=true
+				}
+				else{
+					console.log("Your entry of \""+guess+"\" is not valid. Try again")
+				}
 			}
 		}
-		
+
 		guessedLetters.push(guess)
 
 		if(currentWord.toLowerCase().includes(guess)){
@@ -108,18 +129,36 @@ while(continueGame){
 			console.log(badGuessPhrases[Math.floor(Math.random()*badGuessPhrases.length)]+"\n")
 		}
 
+		//clear and then re-draw the screen
+		console.clear();
+		console.log("Welcome to Hangman!\nPress Ctrl+C to exit at any time\nGames won: "+wonGames+ " Games lost: "+lostGames)
 		console.log(getDrawing(wrongGuesses))
 		console.log(obscuredCW +"          Previous Guesses: "+guessedLetters)
+
+		//the following two if statements check to see if we have hit the end conditions of the game
 		if(!obscuredCW.includes("_")){
-			console.log("Congratulations, you solved it and saved a life!\nGuess it wasn't just luck, this time....\n")
+			if(cheat){
+				console.log("Sure you solved it, but you cheated. Was it worth it for a hollow victory?\n")
+			}
+			else{
+				console.log("Congratulations, you solved it and saved a life!\nGuess it wasn't just luck, this time....\n")
+			}
 			solved=true;
+			wonGames++;
+			let continueInput = await getUserInput("Play again? y/n: ")
+			if(continueInput[0]=="n"){
+				continueGame=false;
+			}
 		}
 		if(wrongGuesses==6){
-			console.log("You failed to solve the word. It was: "+currentWord+"\n\n")
+			lostGames++;
+			console.log("You failed to solve the word. It was: "+currentWord+"\n")
+			let continueInput = await getUserInput("Play again? y/n: ")
+			if(continueInput[0]=="n"){
+				continueGame=false;
+			}
 		}
 	}
 }
-
-
-// console.log(await getUserInput("What is your next guess?"))
+console.clear()
 rl.close()
